e8cee7c6a8e4e84a4d8cacbc66570228f6abe903
Joseph Kuo <chao-chung.kuo@rwth-aachen.de>
2024-02-08 21:10:26 +0100

    update doc

--- genomkit/__init__.py
+++ genomkit/__init__.py
@@ -1,7 +1,7 @@
 from .regions.genomic_region import GRegion
 from .regions.genomic_regions import GRegions
-from .genomic_sequences.genomic_sequence import GSequence
-from .genomic_sequences.genomic_sequences import GSequences
+from .sequences.genomic_sequence import GSequence
+from .sequences.genomic_sequences import GSequences
 
 
 __version__ = "0.1.3"
