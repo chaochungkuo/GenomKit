36f9886740984d66172bb17cad094ca223b7c936
Joseph Kuo <chao-chung.kuo@rwth-aachen.de>
2024-02-08 20:29:59 +0100

    add io for GRegions

--- genomkit/genomic_sequences/__init__.py
+++ genomkit/genomic_sequences/__init__.py
@@ -2,4 +2,10 @@
 Genomic Sequences Modules
 
 These modules contain functions and classes for working with genomic sequences.
+These sequences are usually stored in FASTQ or FASTA files.
+
+- **GSequence** is a single sequence.
+- **GSequences** is a collection of many GSequence objects.
+- **GSequencesSeq** is a set of many GSequences which represent different genomic elements.
+
 """
